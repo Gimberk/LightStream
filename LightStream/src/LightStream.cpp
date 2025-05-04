c96442eed3498a277ef54b30a23b8b29a0521a50
Gimberk <james_f_iii@icloud.com>
2025-05-04 10:42:48 -0400

    Random Image!

--- LightStream/src/LightStream.cpp
+++ LightStream/src/LightStream.cpp
@@ -2,26 +2,59 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
 
-class ExampleLayer : public Walnut::Layer
+using namespace Walnut;
+
+class ExampleLayer : public Layer
 {
 public:
-	virtual void OnUIRender() override
-	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+	virtual void OnUIRender() override {
+		ImGui::Begin("Scene");
+		if (ImGui::Button("Render"))
+			Render();
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+		
+		viewportWidth = ImGui::GetContentRegionAvail().x;
+		viewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if (img)
+			ImGui::Image(img->GetDescriptorSet(), { (float)img->GetWidth(), (float)img->GetHeight() });
+
+		ImGui::End();
+		ImGui::PopStyleVar();
 	}
+
+	void Render() {
+		if (!img || viewportWidth != img->GetWidth() || viewportHeight != img->GetHeight()) {
+			img = std::make_shared<Image>(viewportWidth, viewportHeight, ImageFormat::RGBA);
+			delete[] imageData;
+			imageData = new uint32_t[viewportWidth * viewportHeight];
+		}
+
+		for (uint32_t i = 0; i < viewportWidth * viewportHeight; i++) {
+			imageData[i] = Random::UInt();
+			imageData[i] |= 0xffff00ff;
+		}
+
+		img->SetData(imageData);
+	}
+private:
+	std::shared_ptr<Image> img;
+	uint32_t* imageData = nullptr;
+
+	uint32_t viewportWidth = 0, viewportHeight = 0;
 };
 
-Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+Application* Walnut::CreateApplication(int argc, char** argv)
 {
-	Walnut::ApplicationSpecification spec;
+	ApplicationSpecification spec;
 	spec.Name = "LightStream";
 
-	Walnut::Application* app = new Walnut::Application(spec);
+	Application* app = new Application(spec);
 	app->PushLayer<ExampleLayer>();
 	return app;
 }
